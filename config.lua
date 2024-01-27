225c6645d534c757e8d650aba96825ef235a679f
azutake <azusa_takemae@dwango.co.jp>
2024-01-27 18:27:02 +0900

    add locations

--- config.lua
+++ config.lua
@@ -2,6 +2,42 @@ Config = {}
 Config.Props = {
     ["bennys"] = {
         model = "prop_bin_11b",
-        coords = vector4(-235.33, -1328.39, 31.30, 343.40),
+        coords = vector4(-211.69, -1323.69, 31.30, 235.25),
+        create = true,
     },
+    ["recyclecenter"] = {
+        model = "prop_bin_11b",
+        coords = vector4(59.62, 6475.84, 31.43, 220.08),
+        create = true,
+    },
+    ["lscustoms"] = {
+        model = "prop_bin_11b",
+        coords = vector4(-338.14, -142.81, 39.01, 90.02),
+        create = true,
+    },
+    ["cityservices1"] = {
+        model = "prop_bin_05a",
+        coords = vector4(-238.15707397461, -974.03680419922, 28.289350509644, 346.39273071289),
+        create = false,
+    },
+    ["cityservices2"] = {
+        model = "prop_bin_05a",
+        coords = vector4(-234.24792480469, -962.86053466797, 28.289342880249, 346.40798950195),
+        create = false,
+    },
+    ["cityservices3"] = {
+        model = "prop_bin_01a",
+        coords = vector4(-231.20962524414, -968.49981689453, 28.290952682495, 0.22728180885315),
+        create = false,
+    },
+    ["cityservices4"] = {
+        model = "prop_bin_01a",
+        coords = vector4(-223.17785644531, -946.77301025391, 28.292692184448, 0.64528125524521),
+        create = false,
+    },
+    ["cityservices5"] = {
+        model = "prop_bin_01a",
+        coords = vector4(-243.56227111816, -938.63946533203, 30.218502044678, 0.0),
+        create = false,
+    }
 }
